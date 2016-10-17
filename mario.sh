c5d81f86632c4b107975d53b89c4e53b9e4428a8
chebykinn <ivan@chebykinn.ru>
2016-10-18 00:11:25 +0300

    Updated script indents, and added screen clearing

--- mario.sh
+++ mario.sh
@@ -1,32 +1,32 @@
-#!/usr/bin/env bash
-
+#!/bin/bash
 IFS=''
 CMD=''
 TMP=''
-
+START=0
+clear
+echo "press q to exit"
 export LC_ALL="C"
-(while true;
-do
-  read -s -n 1 -t 1 TMP
-  RES=$?
-  CMD="$CMD$TMP"
-  if [ "$CMD" == '' ]
-  then
-    read -s -n 1 TMP; CMD="$CMD$TMP";
-    if [ "$CMD" == '[' ]
-    then
-      read -s -n 1 TMP; CMD="$CMD$TMP";
-    fi
-  fi
-  if [ $RES -eq 0 ]
-  then
-    echo "$CMD"
-  fi
-
-  if [ "$CMD" == "" ]
-  then
-    echo
-  fi
-  CMD=''
-  TMP=''
+(while true; do
+	if [ $START -eq 0 ]; then
+		START=1
+		echo
+		continue
+	fi
+	read -s -n 1 -t 1 TMP
+	RES=$?
+	CMD="$CMD$TMP"
+	if [ "$CMD" == '' ]; then
+		read -s -n 1 TMP; CMD="$CMD$TMP";
+		if [ "$CMD" == '[' ]; then
+			read -s -n 1 TMP; CMD="$CMD$TMP";
+		fi
+	fi
+	if [ $RES -eq 0 ]; then
+		echo "$CMD"
+	fi
+	if [ "$CMD" == "" ]; then
+		echo
+	fi
+	CMD=''
+	TMP=''
 done) | ./mario.sed
